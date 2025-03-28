4970eae514f5c988cca08bda6727113690d6fbca
lucasd4niel <lucasdanieldmx@gmail.com>
2025-03-28 03:03:55 -0300

    nao deu certo ainda

--- encontro_cidade.c
+++ encontro_cidade.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
 
 int main(void){
-    int hp, mp;
-    float a, b, c, ab, bc, kmp, kmc;
+    int ab, ac, bc, hora_p, minuto_p, hora_c, minuto_c, tempo_chegada_pedro, tempo_chegada_carol;
+    float vel_pedro, vel_carol;
+
+    hora_c = 0;
+    minuto_c = 0;
     /*informa a distancia entre A e B*/
-    scanf("%f", &ab);
+    scanf("%d", &ab);
     
     /*informa a distancia entre B e C*/
-    scanf("%f", &bc);
+    scanf("%d", &bc);
     
     /*Pega a velocidade de pedro*/
-    scanf("%f", &kmp);
-    
+    scanf("%f", &vel_pedro);
+
     /*pega as horas que o pedro partiu*/
-    scanf("%d:%d",&hp, &mp);
-
-
-    /*descobre a distancia entre A e C*/
-    a = ab - bc;
+    scanf("%d:%d",&hora_p, &minuto_p);
     
+    /*descobre a distancia entre A e C*/
+    ac = ab - bc;
+    
+    /*descobre o tempo que o pedro vai levar para chegar em C*/
+    tempo_chegada_pedro = (ac / vel_pedro) * 60;
+   
+    if (tempo_chegada_pedro >= 60 && tempo_chegada_pedro < 120 ){
+        minuto_p = minuto_p + (tempo_chegada_pedro % 10);
+        hora_p = hora_p + tempo_chegada_pedro/60;
+    }
+
+    if (tempo_chegada_pedro >= 120){
+        minuto_p = 0 + (tempo_chegada_pedro % 100);
+        hora_p = hora_p + 1;
+    }
+
+    vel_carol = vel_pedro / 2;
+    tempo_chegada_carol = (bc / vel_carol)*60;
+
+    hora_c = hora_p - tempo_chegada_carol;
+
+
+    printf("%d:%d\n", hora_c, minuto_c);
+
+
     return 0;
-}
+}git config --global user.email "lucasdanieldmx@gmail.com"
+  git config --global user.name "lucasd4niel"
